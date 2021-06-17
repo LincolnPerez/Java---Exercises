772bc12c261f37dfec96c999b676dedf1141e401
Lincoln de Lima Leite <81479637+LincolnPerez@users.noreply.github.com>
2021-06-17 13:18:01 -0300

    Add files via upload

---
+++ Exercise6.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		
+		
+			    Scanner sc = new Scanner(System.in);
+
+				System.out.print("Informe 10 numeros:");
+				int n1 = sc.nextInt();
+				int n2 = sc.nextInt();
+				int n3 = sc.nextInt();
+				int n4 = sc.nextInt();
+				int n5 = sc.nextInt();
+				int n6 = sc.nextInt();
+				int n7 = sc.nextInt();
+				int n8 = sc.nextInt();
+				int n9 = sc.nextInt();
+				int n10 = sc.nextInt();
+
+				int n = n1+n2+n3+n4+n5+n6+n7+n8+n9+n10;
+			
+				System.out.print("A soma dos 10 numeros e: " + n);
+	}
+}
+				
